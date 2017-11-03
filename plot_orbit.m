4ac6d566d4eb9a099a08b6dac38338deadeb1b34
Jeremy Penn <jeremy.penn20@gmail.com>
2017-11-02 22:33:23 -0500

    Add files via upload

--- plot_orbit.m
+++ plot_orbit.m
@@ -1,42 +1,53 @@
-function plot_orbit(h, e, i, omega, w, th, mu)
+function plot_orbit(h, e, i, W, w, th)
     %% Plot the orbits of small mass orbiting a large central body
     %
     % Jeremy Penn
     % 29 October 2017
     %
     % Revision: 29/10/17
+    %           02/11/17 - Added functionality for inputing the state
+    %                      vector directly.
     %
-    % function plot_orbit(h, e, i, omega, w, th, mu)
+    % function plot_orbit(h, e, i, W, w, th)
+    %
+    % OR
+    %
+    % function plot_orbit(h, e, i, W, w, th)
     %
     % Purpose:  This function plots the orbital path of an object orbiting
     %           a central body.
     %
-    % Input:    o h     - Specific angular momentum
-    %           o e     - eccentricity
+    % Input:    o h     - Specific angular momentum or the position vector.
+    %           o e     - eccentricity or the velocity vector.
     %           o i     - orbital inclination
-    %           o omega - right ascension of the ascending node
+    %           o W     - right ascension of the ascending node
     %           o w     - argument of perigee
     %           o th    - true anomaly
-    %           o mu    - standard grav param [OPTIONAL]
     %
     % Requires: rv_from_coe.m
     %
-    
+    % Notes: This program allows the user to either input the coe or the
+    %        state vector.
+    %
     clc;
     
-    if nargin == 6
-        mu = 398600;
+    %% Find r & v
+    if nargin == 2
+        r0 = h;
+        v0 = e;
+    else
+        [r0, v0] = rv_from_coe(h, e, i, W, w, th);
     end
     
-    %% Find r & v
-    [r0, v0] = rv_from_coe(h, e, i, omega, w, th);
+    %% Constants
     
+    mu     = 398600; %[km^3/s^2]
     r      = norm(r0);
     T0     = 2*pi/sqrt(mu)*(r)^(1.5); %[s] period of the orbit
     
     %% Set up initial conditions
     y0 = [r0, v0];
-    t0 = [0 T0];
+    t0 = [0, T0];
     
     %% Numerically solve for the orbit
     [t,y] = ode45(@orbit, t0, y0);
@@ -47,10 +58,10 @@ function plot_orbit(h, e, i, omega, w, th, mu)
     grs80 = referenceEllipsoid('grs80','km');
     
     figure('Renderer','opengl')
-    ax = axesm('globe','Geoid',grs80,'Grid','off', ...
+    axes = axesm('globe','Geoid',grs80,'Grid','off', ...
         'GLineWidth',1,'GLineStyle','-',...
         'Gcolor',[0.9 0.9 0.1],'Galtitude',100);
-    ax.Position = [0 0 1 1];
+    axes.Position = [0 0 1 1];
     axis equal off
     view(0,23.5)
     
@@ -68,39 +79,32 @@ function plot_orbit(h, e, i, omega, w, th, mu)
     end
     
     function [accel] = orbit(t, y)
-        %% Calculates the changing conditions vector for numerical simulation
-        %
-        % Jeremy Penn
-        % 29 October 2017
-        %
-        % Revision: 29/10/2017
-        %
-        % function [accel] = orbit(t, y)
+        %% Calculates the changing conditions vector for numerical simulation)
         %
         % Purpose: To continuously calculate the new conditions vector for use
         %          with ode45.
         %
         
-        mu = 398600; %Gravitational parameter of Earth (km^3/s^2)
+        mu = 398600; %Gravitational parameter of Earth [km^3/s^2]
         
         %% Set initial position and velocity
         
-        rx = y(1); %km
-        ry = y(2); %km
-        rz = y(3); %km
-        vx = y(4); %km/s
-        vy = y(5); %km/s
-        vz = y(6); %km/s
+        rx = y(1); %[km]
+        ry = y(2); %[km]
+        rz = y(3); %[km]
+        vx = y(4); %[km/s]
+        vy = y(5); %[km/s]
+        vz = y(6); %[km/s]
         
         %% Calculate accelerations
         
         R  = norm([rx, ry, rz]);
-        ax = -mu*rx/R^3; %km/s^2
-        ay = -mu*ry/R^3; %km/s^2
-        az = -mu*rz/R^3; %km/s^2
+        ax = -mu*rx/R^3; %[km/s^2]
+        ay = -mu*ry/R^3; %[km/s^2]
+        az = -mu*rz/R^3; %[km/s^2]
         
         %% Set up new conditions after t seconds
         accel = [vx; vy; vz; ax; ay; az];
         
     end %orbit
-end
+end %plot_orbit
