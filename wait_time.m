e5bff147b9a59c148b2d63cf08b4d3af9d0c8290
Jeremy Penn <jeremy.penn20@gmail.com>
2017-11-14 21:10:43 -0600

    Add files via upload

--- wait_time.m
+++ wait_time.m
@@ -1,10 +1,27 @@
-function wait_time(n1, n2, t12)
+function wait_time()
     %% Calculate the wait time after Hohmann transfer for return transfer
     %
     % Jeremy Penn
     % 06/11/17
     %
+    % Required: planet_select.m, time_of_transfer.m
+    %
     clc;
+    
+    %% inputs
+    planet1 = input('Input the departure planet:\n','s');
+    planet2 = input('Input the target planet:\n','s');
+    
+    planet1 = lower(planet1);
+    planet2 = lower(planet2);
+    
+    dp = planet_select(planet1);
+    da = planet_select(planet2);
+    
+    n1 = 2*pi/dp(8);
+    n2 = 2*pi/da(8);
+    
+    t12 = time_of_transfer(dp(5),da(5)) / 86400;
     
     %% calculate the phase angle after transfer
     phif = pi - n1*t12;
