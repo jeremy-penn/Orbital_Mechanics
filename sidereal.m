0e9e9ed4cd9587f71d38572fabdcc1762bd5dae6
Jeremy Penn <jeremy.penn20@gmail.com>
2018-06-24 13:52:25 -0500

    Block commenting

--- sidereal.m
+++ sidereal.m
@@ -1,27 +1,27 @@
 function stime = sidereal(y, m, d,h, min, sec,gam)
     %% Calculate the sidereal time from the given date and UT time
-    %
-    % Jeremy Penn
-    % 21 October 2017
-    %
-    % Revision: 21/10/17
-    %           09/11/17 - replaced while loops with mod to keep ang
-    %                      between 0 and 360
-    %           
-    % function stime = sidereal(y, m, d,h, min, sec)
-    %
-    % Purpose:  This function calculates the sidereal time.
-    % 
-    % Inputs:   o y     - The year
-    %           o m     - The month
-    %           o d     - The day
-    %           o h     - The hour
-    %           o min   - The minutes
-    %           o sec   - The seconds
-    %           o gam   - The eastward longitude
-    %
-    % Outputs:  o stime - The sidereal time
-    %
+    %{
+    Jeremy Penn
+    21 October 2017
+    
+    Revision: 21/10/17
+              09/11/17 - replaced while loops with mod to keep ang
+                         between 0 and 360
+               
+    function stime = sidereal(y, m, d,h, min, sec)
+    
+    Purpose:  This function calculates the sidereal time.
+     
+    Inputs:    o y     - The year
+               o m     - The month
+               o d     - The day
+               o h     - The hour
+               o min   - The minutes
+               o sec   - The seconds
+               o gam   - The eastward longitude
+    
+    Outputs:  o stime - The sidereal time
+    %}
     
     j0 = 367*y - fix(7*(y + fix((m + 9)/12))/4) + fix((275*m)/9) + d + 1721013.5;
     T0 = (j0 - 2451545) / 36525;
